4398e9e57f10e9c555914232397b0224abfa8d46
Sumith Singh <72908644+SumithSingh18@users.noreply.github.com>
2023-03-09 21:36:20 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+var xValues = ["HTML5", "CSS3", "JS", "Bootstrap", "ReactJS"];
+var yValues = [55, 49, 24, 24, 15];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      
+    }
+  }
+});
